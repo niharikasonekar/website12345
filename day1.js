66bd839d56d54abb2be4ae0eb0bec1c22c30e0cb
niharikasonekar <niharika151196@gmil.com>
2025-01-10 16:34:22 +0530

    update

---
+++ day1.js
@@ -0,0 +1,108 @@
+// function sid(){
+//     console.log("hello world")
+// }
+// function abc(){
+//     console.log("second");
+// }
+// sid();
+// abc();
+
+// function go(){
+//     // type coercion
+//     var a="hello";// string
+//     var b=undefined; // nan==>not a number
+//     console.log(a+b);// string
+// }
+
+
+// go ()
+function niharika(){
+// window.alert("this is an alert message")
+// let abc=window.confirm("are you fine")
+// console.log(abc);
+// let a=prompt("are you happy with our website")
+// alert(a);
+// let ans=prompt("what is your name")
+// alert("your name is " + ans)
+// if else
+// let a=6;
+// if(a%2==0){
+//     alert("even number")
+// }
+// else{
+//     alert("odd number")
+// }checking dyanmically even or odd
+// let abc=prompt("enter number to check")
+// if(abc%2==0){
+//     alert("even number")
+// }
+// else{
+//     alert("odd number")
+// }
+// }
+
+// niharika()
+
+}
+// function neena(){
+//     let abc=prompt("enter your age")
+//        if(abc<=18) {
+//         alert("you got vote")
+// }
+// else{
+//     alert("not")
+// }
+  
+
+function neena(){
+
+//     let abc=prompt("enter your age")
+//             (abc>=18)?alert("you got vote"):alert("not")
+                
+// } 
+
+// let a=confirm("you are happy")
+// let abc=prompt("print 1 to 10")
+// for(let i=1;i<=10;++i)
+// console.log(i);
+
+//print 1 to 10 while loop in console
+//  let abc=prompt("print 1 to 10")
+//  while(let i=1;i<=10;i++)
+// //  
+//     console.log(i);
+
+//-----------
+}
+
+
+
+
+neena()
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// }
+
+
+
+
+
+
+
+neena()
---
+++ string.js
@@ -0,0 +1,4 @@
+function niharika(){
+    alert("hello")
+}
+niharika()
