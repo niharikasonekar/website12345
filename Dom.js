e9c737bde2a95813c71ce137c320cc964d07510a
niharikasonekar <niharika151196@gmil.com>
2025-01-18 21:46:18 +0530

    first commit

---
+++ Dom.js
@@ -0,0 +1,16 @@
+// function niharika(){
+//     //document.getElementbById("text")
+//     document.querySelector("#new")
+//     document.innerHTML="Hello i am developer";
+  
+   // para.style.color="red"
+// }
+// day 2
+function textchange(){
+   let change= document.querySelector("#box")
+   change.style.color="yellow"
+//    change.innerHTML=<h1>"hello cybrome" </h1>
+//    change.style.color="red" // to change style
+change.style.backgroundColor="orange"
+
+}
--- string.js
+++ string.js
@@ -1,4 +1,71 @@
+// function niharika(){
+//     alert("hello")
+// }
+// niharika()
+
+
+
+
+
+//day 2//
+
+// function nn(){
+
+// alert("hello") broweser window
+// //console.log
+ 
+// document .write("<h1> hello </h1>")
+
+// let text="this is string"
+// document.write(text.length) to check length of string
+// docment .write(text.slice(4,13))
+// let text="tanisha"
+// let text="shivani"
+// document.write(text1.concat(text2))
+// let world="hellow world";
+// document.write(text.toUpperCase(world))
+
+
+
+
+////day 3--------
+// let text="this is string"
+// document.write(text.indexOf("i",3))
+
+
+// let text1="i am learning javascript and Javascript is fun"
+// document.write(text1.replace,("javascript","JS"))
+
+// document.write(text1.replace(/javascript/g,"JS")) //global=>g i=>case insensitive
+
+// document.write(text1.replace(/javascript/gi,"JS")) //global=>g i=>case insensitive
+
+// console.log(text1.split(" "));
+// let arr=[2,4,5,6,7"color"] // array
+// let text1="i am learning -javascript -and Javas*cript is fun"
+
+// console.log(text1.split("* "));
+
+// let text='java"script" 'is' easy'
+// alert(text)
+// //s.
+// upport single quotes('')and double quotes("")
+
+
+// }
+
+// //  nn();
+
 function niharika(){
-    alert("hello")
+    //arrays//
+    //day 2//
+    let arr1=[1,2,3,4,5]
+    // let arr2=[6,7,8,9]
+    // let arr3=[12,14,15]
+   // console.log(arr1.concat(arr2,arr3));
+//    console.log(reverse(arr1));
+// splice
+// arr1.splice(2,0,9)
+// console.log(arr1);
+console.log(arr1.sort)
 }
-niharika()
