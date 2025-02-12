39321e83772588488b6d2a26ddae310907789001
Niharika <niharika151196@gmail.com>
2025-02-12 22:13:36 +0530

    math.js

---
+++ .vscode/SetTimeout.js
@@ -0,0 +1,38 @@
+setTimeout(()=>{
+    console.log("Running.....");
+ },3000)
+
+console.log("In proccess");
+
+
+let shiv=()=>{
+let show=document.querySelector("#bt1")
+setTimeout(()=>{
+show.innerHTML="shivani"
+},3000)
+}
+
+//===============================SETINTERVAL===============================================
+
+setInterval(()=>{
+    console.log("Running.....");
+},2000)
+
+
+//=========================================COUNT======================================
+// let count=0;
+// setInterval(()=>{
+//     console.log(count);                     //console prrr
+//     count++
+// },2000)
+
+//  let clear;
+// let counta=0;
+// let ram=()=>{
+//     let show=document.querySelector("#bt2")
+//   clear=setInterval(()=>{
+//     show.innerHTML=counta;                     
+//     counta++
+// },2000)
+
+// }
---
+++ Date/script.js
@@ -0,0 +1,50 @@
+console.log(new Date())
+
+
+
+// get full year
+
+let t=new Date()
+console.log(t.getFullYear())
+
+// to readable mode
+
+console.log(t.toDateString())
+
+// get month
+console.log(t.getMonth())
+
+
+// get day
+console.log(t.getDay())
+
+// get hour
+
+console.log(t.getHours())
+
+// get minutes
+
+console.log(t.getMinutes())
+
+// get second
+
+console.log(t.getSeconds())
+
+// get milli second
+
+console.log(t.getMilliseconds())
+
+
+
+// get day in Array form
+
+
+
+let a=["Sun", "Mon","tues", "Wes","thus", "fri","sat"]
+let b=document.querySelector("#s")
+
+let s1=()=>{
+
+    b.innerHTML=a[t.getDay()] 
+}
+
